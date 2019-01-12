4316cc3a07534644caece4320b645101d323ae50
Jürgen Ravnik <ravnikj@gmail.com>
2019-01-12 10:22:58 +0100

    Add test methods for Mul, Div

--- stackCalculator/src/test/java/at/edu/c02/calculator/logic/OperationTest.java
+++ stackCalculator/src/test/java/at/edu/c02/calculator/logic/OperationTest.java
@@ -1,18 +1,20 @@
 package at.edu.c02.calculator.logic;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 import at.edu.c02.calculator.CalculatorException;
 import at.edu.c02.calculator.MathOperation;
 import at.edu.c02.calculator.logic.operations.Add;
+import at.edu.c02.calculator.logic.operations.Div;
+import at.edu.c02.calculator.logic.operations.Mul;
 
 public class OperationTest {
 
 	@Test
 	public void testAdd() throws CalculatorException {
-		
 		//setup
 		MathOperation add = new Add();
 		
@@ -23,6 +25,39 @@ public class OperationTest {
 		assertEquals(7, result, 0.0001);
 	}
 
+	@Test 
+	public void testMult() throws CalculatorException {
+		//setup
+		MathOperation mult = new Mul();
+		
+		//execute 
+		double result = mult.calculate(50, 3);
+		
+		//verify
+		assertEquals(150, result, 3);
+		
+		double result2 = mult.calculate(-30, 30);
+		assertEquals(-900, result2, 3);
 
+		double result3 = mult.calculate(-30, -2);
+		assertEquals(60, result3, 3);
+	}
+	
+	@Test
+	public void testDiv() throws CalculatorException {
+		MathOperation div = new Div();
+		
+		double result = div.calculate(22, 5.5);
+		
+		assertEquals(4, result, 0.0001);
+		
+		try {
+			double result2 = div.calculate(55, 0);
+		
+			fail("Exception expected");
+		} catch (CalculatorException e) {
+			assertEquals("Division by zero", e.getMessage());
+		}
+	}
 	
 }
